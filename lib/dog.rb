cf68e769a10842bf6c6934898703898231887f46
Allen Phillip Pottinger <allenpottinger@gmail.com>
2020-03-23 18:30:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  
+  def save
+    @@all << self.save 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear_all
+    self.all.clear 
+  end
+    
+  def print_all
+    self.all.each do 
+      |name| puts "#{name}"
+    end
+  end
+end
+
+pluto = Dog.new("Pluto")
+fido = Dog.new("Fido")
+maddy = Dog.new("Maddy")
+
+pluto.save
